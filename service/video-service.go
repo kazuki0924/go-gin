b30de10b34a5e5849e91d6270526ce47a7094232
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-02-11 11:09:51 +0900

    add: video service

---
+++ service/video-service.go
@@ -0,0 +1,27 @@
+package service
+
+import "github.com/kazuki0924/go-gin/entity"
+
+// VideoService interface
+type VideoService interface {
+	Save(entity.Video) entity.Video
+	FindAll() []entity.Video
+}
+
+type videoService struct {
+	videos []entity.Video
+}
+
+// New VideoService
+func New() VideoService {
+	return &videoService{}
+}
+
+func (service *videoService) Save(video entity.Video) entity.Video {
+	service.videos = append(service.videos, video)
+	return video
+}
+
+func (service *videoService) FindAll() []entity.Video {
+	return service.videos
+}
