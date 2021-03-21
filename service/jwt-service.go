e5c5c300c2a8d7e13caa6941f09c338ec40be254
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-03-21 16:14:02 +0900

    add: jwtService

---
+++ service/jwt-service.go
@@ -0,0 +1,69 @@
+package service
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JWTService interface {
+	GenerateToken(name string, admin bool) string
+	ValidateToken(tokenString string) (*jwt.Token, error)
+}
+
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+type jwtService struct {
+	secretKey string
+	issuer    string
+}
+
+func getSecretKey() string {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "secret"
+	}
+	return secret
+}
+
+func NewJWTService() JWTService {
+	return &jwtService{
+		secretKey: getSecretKey(),
+		issuer:    "example.com",
+	}
+}
+
+func (jwtSrv *jwtService) GenerateToken(username string, admin bool) string {
+	claims := &jwtCustomClaims{
+		username,
+		admin,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24 * 3).Unix(),
+			Issuer:    jwtSrv.issuer,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte(jwtSrv.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func (jwtSrv *jwtService) ValidateToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(jwtSrv.secretKey), nil
+	})
+}
