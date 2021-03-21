ba24a01414349b4f2af969816e69eb7d3d658570
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-03-22 00:57:27 +0900

    add: test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.4
---
+++ service/video-service_test.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/kazuki0924/go-gin/entity"
+	"github.com/kazuki0924/go-gin/repository"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	TITLE       = "Video Title"
+	DESCRIPTION = "Video Description"
+	URL         = "https://youtu.be/JgW-i2QjgHQ"
+)
+
+func getVideo() entity.Video {
+	return entity.Video{
+		Title:       TITLE,
+		Description: DESCRIPTION,
+		URL:         URL,
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	videoRepository := repository.NewVideoRepository()
+	defer videoRepository.CloseDB()
+
+	videoService := New(videoRepository)
+
+	videoService.Save(getVideo())
+
+	videos := videoService.FindAll()
+
+	firstVideo := videos[0]
+	assert.NotNil(t, videos)
+	assert.Equal(t, TITLE, firstVideo.Title)
+	assert.Equal(t, DESCRIPTION, firstVideo.Description)
+	assert.Equal(t, URL, firstVideo.URL)
+}
